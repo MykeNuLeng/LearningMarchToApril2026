ced660f2bf3cad0f597d3f40dec27595888d85e2
Mike Linford <linford.mike@gmail.com>
2026-03-03 21:43:29 +0000

    week 1 ml complete

--- march/ml/linear_regression_from_scratch/main.py
+++ march/ml/linear_regression_from_scratch/main.py
@@ -1,4 +1,4 @@
-from test_vector import test_vector_addition, test_scalar_multiplication, test_dot_product
+from test_vector import test_vector_addition, test_scalar_multiplication, test_dot_product, test_matrix_multiplication
 
 if __name__ == "__main__":
     print("Running vector tests")
@@ -7,3 +7,5 @@ if __name__ == "__main__":
     test_scalar_multiplication()
     print("running dot product tests")
     test_dot_product()
+    print("Running matrix multiplication tests")
+    test_matrix_multiplication()
--- march/ml/linear_regression_from_scratch/test_vector.py
+++ march/ml/linear_regression_from_scratch/test_vector.py
@@ -1,4 +1,4 @@
-from vector import vector_addition, scalar_multiplication, dot_product
+from vector import vector_addition, scalar_multiplication, dot_product, matrix_multiplication
 
 def test_vector_addition():
     v1 = [1, 2]
@@ -25,4 +25,19 @@ def test_dot_product():
     v2 = [4, 5]
 
     assert dot_product(v1, v2) == 14
+    print("Test passed")
+
+def test_matrix_multiplication():
+    m1 = [[1, 3], [2, 4]]
+    m2 = [[2, 1], [0, 2]]
+
+    assert matrix_multiplication(m1, m2) == [[4, 10], [4, 8]]
+    print("Test passed")
+
+    m3 = [[5, 7], [6, 8]]
+    m4 = [[2, 3], [1, 4]]
+
+    print(f"{matrix_multiplication(m3, m4)}")
+
+    assert matrix_multiplication(m3, m4) == [[28, 38], [29, 39]]
     print("Test passed")
