eea8204c41fc184bdd7878e71e12833e1d0c0b02
Mike Linford <linford.mike@gmail.com>
2026-03-21 19:21:13 +0000

    feat: pet rock now takes user input for actions

--- march/rust/rock/src/main.rs
+++ march/rust/rock/src/main.rs
@@ -1,28 +1,37 @@
+use dialoguer::Select;
+
 fn main() {
     let mut dwayne = Rock {
         name: String::from("Dwayne"),
         interactions: Vec::new(),
     };
 
-    dwayne.interact(Interaction::Poke);
-    dwayne.interact(Interaction::Wave);
-    dwayne.interact(Interaction::Poke);
-    dwayne.interact(Interaction::Pet);
-    dwayne.interact(Interaction::Poke);
+    let labels: Vec<&str> = ACTIONS.iter().map(|label| label.0).collect();
 
-    match dwayne.attitude() {
-        Attitude::Furious => println!("{} is shaking with rage", dwayne.name),
-        Attitude::Blushing => println!("You can't be sure, but it looks like {} is blushing?", dwayne.name),
-        Attitude::Stoic => println!("{} is just sitting there letting nothing affect them, not even the steady march of time", dwayne.name),
-        Attitude::Upset => println!("You can see the top of {} starting to droop, must a {} become a diamond to get any attention?", dwayne.name, dwayne.name),
-        Attitude::Dead => {
-            println!("Oh no...");
-            drop(dwayne);
+    loop {
+        let idx = Select::new()
+            .with_prompt(format!("What will you do to {}", dwayne.name))
+            .items(&labels)
+            .interact()
+            .unwrap();
+
+        match &ACTIONS[idx].1 {
+            Interaction::Pebbles => {
+                println!("Oh no...");
+                break;
+            }
+            Interaction::Pet => dwayne.interact(Interaction::Pet),
+            Interaction::Poke => dwayne.interact(Interaction::Poke),
+            Interaction::Wave => dwayne.interact(Interaction::Wave),
+            Interaction::Ignore => dwayne.interact(Interaction::Ignore),
+            Interaction::Admire => dwayne.interact(Interaction::Admire),
         }
+        
+        dwayne.attitude();
     }
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 enum Interaction {
     Pet,
     Poke,
@@ -32,13 +41,14 @@ enum Interaction {
     Pebbles,
 }
 
-enum Attitude {
-    Furious,
-    Blushing,
-    Stoic,
-    Upset,
-    Dead,
-}
+const ACTIONS: &[(&str, Interaction)] = &[
+    ("Pet", Interaction::Pet),
+    ("Poke", Interaction::Poke),
+    ("Wave", Interaction::Wave),
+    ("Ignore", Interaction::Ignore),
+    ("Admire", Interaction::Admire),
+    ("Pebbles", Interaction::Pebbles),
+];
 
 struct Rock {
     name: String,
@@ -50,19 +60,20 @@ impl Rock {
         self.interactions.push(interaction)
     }
 
-    fn attitude(&self) -> Attitude {
-        let total = self.interactions.len().min(7);
-        let slice = &self.interactions[..total];
+    fn attitude(&self) {
+        let start = if self.interactions.len() < 7 {0} else {self.interactions.len() - 7};
+        let slice = &self.interactions[start..];
 
-        if slice.iter().filter(|&x| x == &Interaction::Pebbles).count() >= 1 {
-            return Attitude::Dead;
-        } else if slice.iter().filter(|&x| x == &Interaction::Poke).count() >= 3 {
-            return Attitude::Furious;
+        if slice.iter().filter(|&x| x == &Interaction::Poke).count() >= 3 {
+            println!("{} is shaking with rage", self.name);
         } else if slice.iter().filter(|&x| x == &Interaction::Ignore).count() >= 3 {
-            return Attitude::Upset;
+            println!("You can see the top of {} starting to droop, must {} become a diamond to get any attention?", self.name, self.name);
         } else if slice.iter().filter(|&x| x == &Interaction::Admire).count() >= 4 {
-            return Attitude::Blushing;
+            println!("You can't be sure, but it looks like {} is blushing?", self.name);
+        } else if slice.iter().filter(|&x| x == &Interaction::Pet).count() >= 2 {
+            println!("{} looks oddly content for a rock", self.name);
+        } else {
+            println!("{} is just sitting there letting nothing affect them, not even the steady march of time", self.name);
         }
-        Attitude::Stoic
     }
 }
